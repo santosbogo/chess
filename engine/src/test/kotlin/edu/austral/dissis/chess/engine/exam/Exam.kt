74895e7bbc36441a9cb7b2d53776bd76453b82ea
santosbogo <sbogo@mail.austral.edu.ar>
2024-05-29 00:10:14 -0300

    Engine has nothing particular from chess. Start the checkers

---
+++ engine/src/test/kotlin/edu/austral/dissis/chess/engine/exam/Exam.kt
@@ -0,0 +1,18 @@
+package edu.austral.dissis.chess.engine.exam
+
+import edu.austral.dissis.chess.games.classicChess.ClassicChess
+import edu.austral.dissis.chess.test.game.GameTester
+import edu.austral.dissis.engine.exam.DummyTestGameRunner
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
+import java.util.stream.Stream
+
+class Exam {
+    @TestFactory
+    fun `required exam tests`(): Stream<DynamicTest> {
+        return GameTester(
+            DummyTestGameRunner(
+            ClassicChess().generateGame())
+        ).test()
+    }
+}
